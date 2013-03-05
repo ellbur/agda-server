ace7bd4c50298f212c7f8b87bef308a9e932020d
Will Jones <will@sacharissa.co.uk>
2013-03-05 12:58:05 +0000

    Hack in some basic types and Aeson instances.

--- src/Agda/Interaction/ServerInteractionTop.hs
+++ src/Agda/Interaction/ServerInteractionTop.hs
@@ -1,3 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Agda.Interaction.ServerInteractionTop where
 
-import Text.XML.Light
+import Control.Applicative
+import Control.Monad
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Text
+
+data Request
+  = LoadR   { rFilePath :: FilePath
+            }
+
+  | GoalsR
+  | GoalR
+  deriving (Eq, Show)
+
+instance FromJSON Request where
+  parseJSON (Object o)
+    = o .: "type" >>= parseRequest o
+
+  parseJSON _
+    = mzero
+
+parseRequest :: Object -> Text -> Parser Request
+parseRequest o "load"
+  = LoadR <$> o .: "filePath"
+
+parseRequest o "goals"
+  = pure GoalsR
+
+parseRequest o "goal"
+  = pure GoalR
+
+parseRequest o _
+  = mzero
