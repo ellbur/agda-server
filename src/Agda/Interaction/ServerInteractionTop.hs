cd41c1653f3faeea7523d918f3abfc12c69f4f52
Will Jones <will@sacharissa.co.uk>
2013-03-11 19:29:31 +0000

    Flesh out the ServerInteractionTop module.

--- src/Agda/Interaction/ServerInteractionTop.hs
+++ src/Agda/Interaction/ServerInteractionTop.hs
@@ -4,6 +4,7 @@ module Agda.Interaction.ServerInteractionTop where
 
 import Agda.Interaction.InteractionTop
 import Agda.Interaction.Response
+import Agda.TypeChecking.Monad.Base
 
 import Control.Applicative
 import Control.Monad
@@ -13,29 +14,38 @@ import Data.Text
 import Web.Scotty
 
 data Request
-  = LoadR FilePath
+  = RequestR FilePath RequestType
+  deriving (Eq, Show)
+
+data RequestType
+  = LoadR
   | GoalsR
   | GoalR
   deriving (Eq, Show)
 
 instance FromJSON Request where
   parseJSON (Object o)
-    = o .: "type" >>= parseRequest o
+    = do
+        ty <- o .: "type"
+        filePath <- o .: "filePath"
+
+        RequestR filePath <$>
+          parseRequestType ty filePath o
 
   parseJSON _
     = mzero
 
-parseRequest :: Object -> Text -> Parser Request
-parseRequest o "load"
-  = LoadR <$> o .: "filePath"
+parseRequestType :: String -> FilePath -> Object -> Parser RequestType
+parseRequestType "load" filePath o
+  = pure LoadR
 
-parseRequest o "goals"
+parseRequestType "goals" filePath o
   = pure GoalsR
 
-parseRequest o "goal"
+parseRequestType "goal" filePath o
   = pure GoalR
 
-parseRequest o _
+parseRequestType ty filePath o
   = mzero
 
 typedObject :: Text -> [Pair] -> Value
@@ -49,12 +59,22 @@ instance ToJSON Response where
   toJSON _
     = object []
 
-interpret :: Request -> IO Response
-interpret (LoadR filePath)
-  = undefined
+requestInteraction :: Request -> Interaction
+requestInteraction (RequestR filePath LoadR)
+  = Cmd_load filePath []
 
-interpret GoalsR
-  = undefined
+requestInteraction (RequestR filePath GoalsR)
+  = Cmd_metas
 
-interpret GoalR
-  = undefined
+requestInteraction (RequestR filePath GoalR)
+  = Cmd_metas
+
+requestIOTCM :: Request -> IOTCM
+requestIOTCM rq@(RequestR filePath ty)
+  = IOTCM filePath NonInteractive Direct $
+      requestInteraction rq
+
+runIOTCM :: CommandState -> IOTCM -> IO CommandState
+runIOTCM st ioTCM
+  = either (const st) snd <$>
+      runTCM (runCommandM (runInteraction ioTCM) st)
--- src/AgdaServer.hs
+++ src/AgdaServer.hs
@@ -1,6 +1,15 @@
 module AgdaServer where
 
+import Agda.Interaction.InteractionTop
+import Agda.Interaction.ServerInteractionTop
+
+import Data.IORef
+
 main :: IO ()
 main
   = do
+      ref <- newIORef initCommandState
+
+
+
       print ()
